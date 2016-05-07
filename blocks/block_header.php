78f18219cef711128069188038a06be6aa4a3064
Afshin Akhgar <akhgar-af@iransystem.local>
2016-05-07 18:10:03 +0430

    new init

---
+++ blocks/block_header.php
@@ -0,0 +1,8 @@
+<div class="header__topbar">
+	
+</div>
+
+
+<div class="header__navigation">
+	asdasd
+</div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>
+			Rosan Socks
+		</title>
+		<?php include('blocks/head.php');?>
+
+	</head>
+	<body>
+		<?php include('blocks/block_header.php');?>
+
+		<div class="wrapper">
+			asdasdasd
+		</div>
+	</body>
+
+</html>
