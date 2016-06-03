1a411feeeb52115d6662d1053d0eebcac3bcf143
afshin <afshin@akhgar.net>
2016-06-03 18:02:54 +0430

    add tab to categories first page

--- blocks/categories_home_section.php
+++ blocks/categories_home_section.php
@@ -4,5 +4,27 @@
 		<h1>دسته بنده محصولات</h1>
 	</div>
 
+	<div class="categories__section--tab wrapper">
+		<ul class="nav nav-tabs">
+		  <li class="active"><a data-toggle="tab" href="#girl">مردونه</a></li>
+		  <li><a data-toggle="tab" href="#woman">زنونه</a></li>
+		  <li><a data-toggle="tab" href="#man">دخترونه</a></li>
+		</ul>
+
+		<div class="tab-content">
+		  <div id="girl" class="tab-pane fade in active">
+		    <h3>HOME</h3>
+		    <p>Some content.</p>
+		  </div>
+		  <div id="woman" class="tab-pane fade">
+		    <h3>Menu 1</h3>
+		    <p>Some content in menu 1.</p>
+		  </div>
+		  <div id="man" class="tab-pane fade">
+		    <h3>Menu 1</h3>
+		    <p>Some content in menu 1.</p>
+		  </div>
+		</div>
+	</div>	
 	
 </div>
--- blocks/head.php
+++ blocks/head.php
@@ -1,9 +1,10 @@
 <link rel="stylesheet" href="vendor/bootstrap/dist/css/bootstrap.css">
 <link rel="stylesheet" href="vendor/bootstrap/dist/css/bootstrap-theme.css">
 <link rel="stylesheet" href="public/assets/fonticon/css/fontello.css">
-<link rel="stylesheet" href="public/assets/css/src/app.css">
 
 
 <script src="/vendor/jquery/dist/jquery.js"></script>
+<script src="/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
 
 
+<link rel="stylesheet" href="public/assets/css/src/app.css">
