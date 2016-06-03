ec7d381fc0b4506116035f4e543ac71d1e963d16
afshin <afshin@akhgar.net>
2016-06-03 18:09:02 +0430

    fix section

--- blocks/categories_home_section.php
+++ blocks/categories_home_section.php
@@ -1,4 +1,4 @@
-<div id="categories_section" class="categories_section">
+<div id="categories_section section" class="categories_section">
 	
 	<div class="wrapper content_categories">  
 		<h1>دسته بنده محصولات</h1>
